7674ba831764929714f9a2b0128236cd2d784773
Sodayooodev <sodayooo.dev@gmail.com>
2026-08-29 10:12:44 +0800

    transmission 2026-08-29

--- barangan_jhezriel_jay/server/config/settings.py
+++ barangan_jhezriel_jay/server/config/settings.py
@@ -104,5 +104,5 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
 	'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
 	'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-	'AUTH_HEADER_NAME': ('Bearer',),
+	'AUTH_AUTHENTICATION_TYPE': ('Bearer',),
 }
