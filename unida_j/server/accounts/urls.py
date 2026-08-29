cb7e8531b8e2c2f7f76bddce25bfa61e671a909b
Edison Baccay <edisonbaccaymoy1230@gmail.com>
2026-08-29 08:49:17 +0800

    transmission 2026-08-29

--- unida_j/server/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-	path('login/', AccountLogin.as_view()),
-	path('register/', AccountRegister.as_view()),
-	path('accounts/', AccountList.as_view())
-]
