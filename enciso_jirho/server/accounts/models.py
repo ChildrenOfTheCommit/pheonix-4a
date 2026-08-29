c8c54b87643a88da20dd169bb1af6faa4e225eb5
Jirho-BE <jirhoenciso1204@gmail.com>
2026-08-29 08:19:46 +0800

    Added the server files

---
+++ enciso_jirho/server/accounts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from roles.models import Roles
+
+
+class Accounts(AbstractUser):
+	codename = models.CharField(max_length=100, unique=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	role = models.ForeignKey(
+		Roles,
+		on_delete=models.PROTECT
+	)
