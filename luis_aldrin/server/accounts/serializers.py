628856ecf9acdfb1a7959c1087618cced48ad31a
Ainnz Luis <luisaldin2005@gmail.com>
2026-08-29 08:54:50 +0800

    transmission 2026-08-29

---
+++ luis_aldrin/server/accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Accounts
+
+
+class AccountSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Accounts
+		fields = '__all__'
+		extra_kwargs = {
+			'password': {
+				'write_only': True
+			}
+		}
