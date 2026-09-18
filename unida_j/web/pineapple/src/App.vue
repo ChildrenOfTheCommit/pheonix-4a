a9fbee548dd94e5726c7e2eda632e1a29118a36a
nigs <unidacedrick80@gmail.com>
2026-09-19 02:06:11 +0800

    web act 19/09/26

---
+++ unida_j/web/pineapple/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWord from './components/HelloWord.vue'
+</script>
+
+<template>
+  <div class="text-center text-red-500 text-2xl">
+    <HelloWord />
+  </div>
+</template> 
+
+<style></style>
--- unida_j/web/pineapple/vite.config.ts
+++ unida_j/web/pineapple/vite.config.ts
@@ -2,20 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite"
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/',
-	server: {
-		host: '0.0.0.0'
-	}
 })
