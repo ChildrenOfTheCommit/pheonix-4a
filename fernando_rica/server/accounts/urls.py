16b7bf4f6ace2ff318d1ea4ed1d95e64c470764e
Rica Fernando <callosrica@gmail.com>
2026-09-19 11:24:51 +0700

    web

--- fernando_rica/server/accounts/urls.py
+++ fernando_rica/server/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('accounts/', AccountLogin.as_view()),
+	path('login/', AccountLogin.as_view()),
     path('register/',AccountRegister.as_view())
 
 ]
--- fernando_rica/server/config/settings.py
+++ fernando_rica/server/config/settings.py
@@ -94,7 +94,7 @@ AUTH_USER_MODEL= 'accounts.Accounts'
 
 REST_FRAMEWORK={
     'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticated',
