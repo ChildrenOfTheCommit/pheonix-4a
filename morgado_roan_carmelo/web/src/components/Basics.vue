944f7022b88ec0d2a49eadf680da9c63a5792bf7
Roan Carmelo G. Morgado <roancarmelomorgado@gmail.com>
2026-09-19 11:55:53 +0800

    Added web pineapple

---
+++ morgado_roan_carmelo/web/src/components/Basics.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+let accounts = {
+  username: "roanmorgado",
+  password: "1234",
+  displayName : "Melo"
+}
+console.log(accounts)
+
+interface student {
+  username : string
+  password : string
+  displayName : string
+}
+
+const regularStudents: student = {
+  username: "roanmorgado",
+  password: "1234",
+  displayName : "Melo"
+}
+</script>
+
+<template>
+  <div>
+    Hello Vue!!
+    <p class = "pra">2026-09-19</p>
+  </div>
+</template>
+
+<style>
+*{
+  font-family: "Courier New", Courier, monospace;
+  color: purple;
+}
+
+.pra{
+  text-align: center;
+  padding: 20px 10px;
+  background: aqua;
+  corner-block-end-shape: round;
+
+}
+</style>
---
+++ morgado_roan_carmelo/web/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+
+
+<template>
+  <div class = "text-purple-500
+  text-2xl
+  dark:">
+    Hello world
+  </div>
+</template>
