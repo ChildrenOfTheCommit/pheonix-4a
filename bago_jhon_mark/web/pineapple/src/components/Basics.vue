abe72725efc3693ec8bd57ad01851b533c62fd8b
kkkyosshi <jhonmarkbago@gmail.com>
2026-09-19 14:04:43 +0800

    web

---
+++ bago_jhon_mark/web/pineapple/src/components/Basics.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+  //console.log('Hello Typescript.')
+
+  // let number=20
+  // console.log(number)
+
+  //json from api
+  // const students = {
+  //   username: 'fern',
+  //   password: '12345',
+  //   displayName: 'Fern',
+  //   isCute: true,
+  //   age: 100
+  // }
+  // console.log(students)
+
+  // //class or struct
+  // interface students = {
+  //   username: string
+  //   password: number
+  //   displayName: string
+  //   isCute: boolean
+  //   age: number
+  // }
+</script>
+
+<template>
+  <div>
+    Hello VUE!
+    <p>2026-09-19</p>
+  </div>
+</template>
+
+<style>
+  *{
+    font-family: 'Courier New', Courier, monospace;
+    color: purple;
+  };
+</style>
---
+++ bago_jhon_mark/web/pineapple/src/components/HelloWorld.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class = "text-pink-600
+    text-2xl">Hello World</div>
+</template>
